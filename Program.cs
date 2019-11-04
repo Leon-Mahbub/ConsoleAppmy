7785ef303e733f48fadd9685bedd8eee790ba0c7
Mahbub <imleon2010@gmail.com>
2019-11-04 17:58:10 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppmy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
